912c1fec73ccdfa04eadd7917619f6ee13261bb2
Rob Foulkrod <robfoulkrod@microsoft.com>
2024-07-02 14:36:16 -0400

    Adding tests

---
+++ tests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using System.IO;
+
+namespace AwesomeSauceTests
+{
+    public class ReadmeTests
+    {
+        [Fact]
+        public void ReadmeIsSpelledRight()
+        {
+            
+            Assert.True(true);
+        }
+    }
+}
